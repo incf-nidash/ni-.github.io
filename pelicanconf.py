1fd532b4126541f983cd56d1df91c661e5d3b285
Nolan Nichols <nolan.nichols@gmail.com>
2013-11-11 10:30:51 -0800

    add: CNAME paths

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,6 +11,9 @@ TIMEZONE = 'UTC'
 
 DEFAULT_LANG = u'en'
 
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
